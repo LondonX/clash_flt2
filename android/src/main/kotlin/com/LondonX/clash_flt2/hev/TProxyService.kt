5cda1ee61af91f72d7c5b18b61403edc0206f544
LondonX <london@LondonX-iMac.local>
2024-09-20 18:00:28 +0800

    fix NoSuchMethodError in release build

--- android/src/main/kotlin/com/LondonX/clash_flt2/hev/TProxyService.kt
+++ android/src/main/kotlin/com/LondonX/clash_flt2/hev/TProxyService.kt
@@ -3,6 +3,7 @@ package com.LondonX.clash_flt2.hev
 object TProxyService {
     init {
         System.loadLibrary("hev-socks5-tunnel")
+        TProxyGetStats()
     }
     external fun TProxyStartService(config_path: String, fd: Int)
     external fun TProxyStopService()

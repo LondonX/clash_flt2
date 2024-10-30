68a66fb310fe092c292d7c3b9e6481443533cb39
kaes1a <LeeHnCg@gmail.com>
2024-10-30 14:28:11 +0800

    add some build check for target of android

--- fclash/build_libclash.sh
+++ fclash/build_libclash.sh
@@ -21,10 +21,30 @@ GOOS=linux CGO_ENABLED=1 GOARCH=amd64 CC=x86_64-linux-musl-gcc  CXX=x86_64-linux
 
 # Android
 echo "Building Android libclash.so"
+# Check if ANDROID_HOME is set
+if [ -z "$ANDROID_HOME" ]; then
+  echo "Error: ANDROID_HOME is not set. Please set the ANDROID_HOME environment variable."
+  exit 1
+fi
+
+# Check if $ANDROID_HOME/ndk/ directory exists
+if [ ! -d "$ANDROID_HOME/ndk/" ]; then
+  echo "Error: $ANDROID_HOME/ndk/ directory does not exist. Please download the NDK from https://developer.android.com/ndk/downloads/ and extract it to $ANDROID_HOME/ndk/ ."
+  exit 1
+fi
+
+# Find the latest NDK version
+NDK_PATH=$(ls -d $ANDROID_HOME/ndk/* | sort -V | tail -n 1)
+
+# Check if NDK_PATH is a valid directory
+if [ ! -d "$NDK_PATH" ]; then
+  echo "Error: NDK_PATH ($NDK_PATH) is not a valid directory."
+  exit 1
+fi
 echo "  > Build for armeabi-v7a"
-GOOS=android CGO_ENABLED=1 GOARCH=arm GOARM=7 CC=/Users/london/Library/Android/sdk/ndk/26.0.10792818/toolchains/llvm/prebuilt/darwin-x86_64/bin/armv7a-linux-androideabi26-clang go build -ldflags="-w -s" -buildmode=c-shared -o ../dist/android/armeabi-v7a/libclash.so
+GOOS=android CGO_ENABLED=1 GOARCH=arm GOARM=7 CC=$NDK_PATH/toolchains/llvm/prebuilt/darwin-x86_64/bin/armv7a-linux-androideabi26-clang go build -ldflags="-w -s" -buildmode=c-shared -o ../dist/android/armeabi-v7a/libclash.so
 echo "  > Build for arm64-v8a"
-GOOS=android CGO_ENABLED=1 GOARCH=arm64 CC=/Users/london/Library/Android/sdk/ndk/26.0.10792818/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android34-clang go build -ldflags="-w -s" -buildmode=c-shared -o ../dist/android/arm64-v8a/libclash.so
+GOOS=android CGO_ENABLED=1 GOARCH=arm64 CC=$NDK_PATH/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android34-clang go build -ldflags="-w -s" -buildmode=c-shared -o ../dist/android/arm64-v8a/libclash.so
 
 # iOS-dylib
 # echo "Building iOS libclash-ios.dylib"
